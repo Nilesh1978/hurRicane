a98993abf5ab4b0da088989ebf96de57c1bf3b6d
Josh Newkirk FCAS <joshuanewkirk@uwalumni.com>
2017-04-14 23:25:07 -0500

    grid.arrange

--- Assignment staging script.R
+++ Assignment staging script.R
@@ -6,6 +6,7 @@ setwd(pathname)
 library(dplyr)
 library(ggmap)
 library(hurRicane)
+library(gridExra)
 
 #Import data
 ext_tracks<-read_ext_tracks(file="ebtrk_atlc_1988_2015.txt")
@@ -17,7 +18,7 @@ tidy_ext_tracks<-tidy_tracks(ext_tracks=ext_tracks)
 hur_name<-"IKE"
 hur_year<-2008
 hur_month<-"09"
-hur_day<-c("11")
+hur_day<-c("12")
 hur_hour<-c("12")
 
 hur_datehour<-lubridate::ymd_h(paste(hur_year,hur_month,hur_day,hur_hour))
@@ -38,13 +39,13 @@ ggmap(extent = "device")+geom_hurricane(data=storm_observation,
                                                     fill=as.factor(wind_speed),
                                                     color=as.factor(wind_speed)))+
                             scale_color_manual(name = "Wind speed (kts)", 
-                                                 values = c("red", "orange", "yellow")) + 
+                                 values = c("red", "orange", "yellow")) + 
                           scale_fill_manual(name = "Wind speed (kts)", 
-                                                            values = c("red", "orange", "yellow"))
-p  
-
-#This shows how the scale_radii works
-p<-get_map(location=myloc,
+                                 values = c("red", "orange", "yellow"))+
+                          ggtitle("scale_radii=1.00")
+                          
+#This section will show scale down
+p2<-get_map(location=myloc,
            zoom =6 , maptype = "toner-background") %>%
   ggmap(extent = "device")+geom_hurricane(data=storm_observation,
                                           mapping=aes(x=longitude,y=latitude,
@@ -53,9 +54,11 @@ p<-get_map(location=myloc,
                                                       quadrant=quadrant,
                                                       fill=as.factor(wind_speed),
                                                       color=as.factor(wind_speed),
-                                                      scale_radii=.25))+
-  scale_color_manual(name = "Wind speed (kts)", 
-                     values = c("red", "orange", "yellow")) + 
-  scale_fill_manual(name = "Wind speed (kts)", 
-                    values = c("red", "orange", "yellow"))
-p  
+                                                      scale_radii=.55))+
+                     scale_color_manual(name = "Wind speed (kts)", 
+                                values = c("red", "orange", "yellow")) + 
+                    scale_fill_manual(name = "Wind speed (kts)", 
+                                values = c("red", "orange", "yellow")) +
+                    ggtitle("scale_radii = 55%")
+
+gridExtra::grid.arrange(p,p2,ncol=2,nrow=1)
